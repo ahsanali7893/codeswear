d3dfa6fe773eebc40f3a43d825eb80c75e257ac2
ahsanali7893 <ahsan3916902@gmail.com>
2023-06-05 17:36:24 +0500

    initial changes

--- src/pages/stickers.js
+++ src/pages/stickers.js
@@ -1,10 +1,199 @@
 import React from 'react'
+import "../app/globals.css";
+import Footer from 'components/Footer'
+import Navbar from 'components/Navbar'
+import Link from 'next/link'
 
 const stickers = () => {
   return (
+    <>
+    <Navbar />
     <div>
-      
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4">
+            <Link
+              href={"/product/wear-the-code"}
+              className="lg:w-1/5 md:w-1/2 p-4 w-full block relative rounded overflow-hidden shadow-lg m-5"
+            >
+              
+                <img
+                  alt="ecommerce"
+                  className="m-auto md:mx-0 block"
+                  src="https://m.media-amazon.com/images/I/81mBgtDkV4L._AC_SX569_.jpg"
+                />
+             
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  Stickers
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear The Code.
+                </h2>
+                <p className="mt-1">₨ 500</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+            </Link>
+            <Link
+              href={"/product/wear-the-code"}
+              className="lg:w-1/5 md:w-1/2 p-4 w-full block relative rounded overflow-hidden shadow-lg m-5"
+            >
+              
+                <img
+                  alt="ecommerce"
+                  className="m-auto md:mx-0 block"
+                  src="https://m.media-amazon.com/images/I/81mBgtDkV4L._AC_SX569_.jpg"
+                />
+              
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  Stickers
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear The Code.
+                </h2>
+                <p className="mt-1">₨ 500</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+            </Link>
+            <Link
+              href={"/product/wear-the-code"}
+              className="lg:w-1/5 md:w-1/2 p-4 w-full block relative rounded overflow-hidden shadow-lg m-5"
+            >
+              
+                <img
+                  alt="ecommerce"
+                  className="m-auto md:mx-0 block"
+                  src="https://m.media-amazon.com/images/I/81mBgtDkV4L._AC_SX569_.jpg"
+                />
+             
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  Stickers
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear The Code.
+                </h2>
+                <p className="mt-1">₨ 500</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+            </Link>
+            <Link
+              href={"/product/wear-the-code"}
+              className="lg:w-1/5 md:w-1/2 p-4 w-full block relative rounded overflow-hidden shadow-lg m-5"
+            >
+              
+                <img
+                  alt="ecommerce"
+                  className="m-auto md:mx-0 block"
+                  src="https://m.media-amazon.com/images/I/81mBgtDkV4L._AC_SX569_.jpg"
+                />
+             
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  Stickers
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear The Code.
+                </h2>
+                <p className="mt-1">₨ 500</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+            </Link>
+            <Link
+              href={"/product/wear-the-code"}
+              className="lg:w-1/5 md:w-1/2 p-4 w-full block relative rounded overflow-hidden shadow-lg m-5"
+            >
+              
+                <img
+                  alt="ecommerce"
+                  className="m-auto md:mx-0 block"
+                  src="https://m.media-amazon.com/images/I/81mBgtDkV4L._AC_SX569_.jpg"
+                />
+              
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  Stickers
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear The Code.
+                </h2>
+                <p className="mt-1">₨ 500</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+            </Link>
+            <Link
+              href={"/product/wear-the-code"}
+              className="lg:w-1/5 md:w-1/2 p-4 w-full block relative rounded overflow-hidden shadow-lg m-5"
+            >
+              
+                <img
+                  alt="ecommerce"
+                  className="m-auto md:mx-0 block"
+                  src="https://m.media-amazon.com/images/I/81mBgtDkV4L._AC_SX569_.jpg"
+                />
+              
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  Stickers
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear The Code.
+                </h2>
+                <p className="mt-1">₨ 500</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+            </Link>
+            <Link
+              href={"/product/wear-the-code"}
+              className="lg:w-1/5 md:w-1/2 p-4 w-full block relative rounded overflow-hidden shadow-lg m-5"
+            >
+              
+                <img
+                  alt="ecommerce"
+                  className="m-auto md:mx-0 block"
+                  src="https://m.media-amazon.com/images/I/81mBgtDkV4L._AC_SX569_.jpg"
+                />
+             
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  Stickers
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear The Code.
+                </h2>
+                <p className="mt-1">₨ 500</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+            </Link>
+            <Link
+              href={"/product/wear-the-code"}
+              className="lg:w-1/5 md:w-1/2 p-4 w-full block relative rounded overflow-hidden shadow-lg m-5"
+            >
+              
+                <img
+                  alt="ecommerce"
+                  className="m-auto md:mx-0 block"
+                  src="https://m.media-amazon.com/images/I/81mBgtDkV4L._AC_SX569_.jpg"
+                />
+             
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  Stickers
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear The Code.
+                </h2>
+                <p className="mt-1">₨ 500</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </section>
     </div>
+    <Footer />
+  </>
   )
 }
 
