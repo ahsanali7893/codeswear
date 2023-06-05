cea5b73c324ca41d3aec5a98f9ed1ed7973aac4e
ahsanali7893 <ahsan3916902@gmail.com>
2023-06-05 13:02:37 +0500

    initial changes

--- src/pages/tshirts.js
+++ src/pages/tshirts.js
@@ -12,8 +12,8 @@ const tshirts = () => {
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-wrap -m-4">
           <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260" />
+            <a className="block relative rounded overflow-hidden">
+              <img alt="ecommerce" className=" h-[36vh] block" src="https://m.media-amazon.com/images/I/61KtrldtmeL._AC_UY550_.jpg" />
             </a>
             <div className="mt-4">
               <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
@@ -22,8 +22,8 @@ const tshirts = () => {
             </div>
           </div>
           <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/421x261" />
+            <a className="block relative rounded overflow-hidden">
+              <img alt="ecommerce" className=" h-[36vh] block" src="https://m.media-amazon.com/images/I/61KtrldtmeL._AC_UY550_.jpg" />
             </a>
             <div className="mt-4">
               <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
@@ -32,8 +32,8 @@ const tshirts = () => {
             </div>
           </div>
           <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/422x262" />
+            <a className="block relative rounded overflow-hidden">
+              <img alt="ecommerce" className=" h-[36vh] block" src="https://m.media-amazon.com/images/I/61KtrldtmeL._AC_UY550_.jpg" />
             </a>
             <div className="mt-4">
               <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
@@ -42,8 +42,8 @@ const tshirts = () => {
             </div>
           </div>
           <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/423x263" />
+            <a className="block relative rounded overflow-hidden">
+              <img alt="ecommerce" className=" h-[36vh] block" src="https://m.media-amazon.com/images/I/61KtrldtmeL._AC_UY550_.jpg" />
             </a>
             <div className="mt-4">
               <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
@@ -52,8 +52,8 @@ const tshirts = () => {
             </div>
           </div>
           <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/424x264" />
+            <a className="block relative rounded overflow-hidden">
+              <img alt="ecommerce" className=" h-[36vh] block" src="https://m.media-amazon.com/images/I/61KtrldtmeL._AC_UY550_.jpg" />
             </a>
             <div className="mt-4">
               <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
@@ -62,8 +62,8 @@ const tshirts = () => {
             </div>
           </div>
           <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/425x265" />
+            <a className="block relative rounded overflow-hidden">
+              <img alt="ecommerce" className=" h-[36vh] block" src="https://m.media-amazon.com/images/I/61KtrldtmeL._AC_UY550_.jpg" />
             </a>
             <div className="mt-4">
               <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
@@ -72,18 +72,18 @@ const tshirts = () => {
             </div>
           </div>
           <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/427x267" />
+            <a className="block relative rounded overflow-hidden">
+              <img alt="ecommerce" className=" h-[36vh] block" src="https://m.media-amazon.com/images/I/61KtrldtmeL._AC_UY550_.jpg" />
             </a>
             <div className="mt-4">
               <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
               <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
-              <p className="mt-1">$12.00</p>
+              <p className="mt-1">$12.00</p>  
             </div>
           </div>
           <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/428x268" />
+            <a className="block relative rounded overflow-hidden">
+              <img alt="ecommerce" className=" h-[36vh] block" src="https://m.media-amazon.com/images/I/61KtrldtmeL._AC_UY550_.jpg" />
             </a>
             <div className="mt-4">
               <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
