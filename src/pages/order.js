d82a474494a59c21d396ba5955b3a608bf5c2352
ahsanali7893 <ahsan3916902@gmail.com>
2023-06-12 16:51:39 +0500

    Order Page completer

--- src/pages/order.js
+++ src/pages/order.js
@@ -21,13 +21,13 @@ const Order = () => {
                 Your order has been successfully placed
               </p>
               <div className="flex mb-4">
-                <a className="flex-grow text-pink-500 border-b-2 border-pink-500 py-2 text-lg px-1">
+                <a className="flex-grow text-center  border-pink-500 py-2 text-lg px-1">
                   Description
                 </a>
-                <a className="flex-grow border-b-2 border-gray-300 py-2 text-lg px-1">
+                <a className="flex-grow text-center py-2 text-lg px-1">
                   Quantity
                 </a>
-                <a className="flex-grow border-b-2 border-gray-300 py-2 text-lg px-1">
+                <a className="flex-grow text-center py-2 text-lg px-1">
                   Items Total
                 </a>
               </div>
@@ -46,10 +46,15 @@ const Order = () => {
                 <span className="ml-auto text-gray-900">1</span>
                 <span className="ml-auto text-gray-900">499</span>
               </div>
-              <div className="flex">
+              <div className="flex flex-col">
                 <span className="title-font font-medium text-2xl text-gray-900">
-                  Subtotal: $58.00
+                  Subtotal: ₨ 499
                 </span>
+                <div className="my-2">
+                  <button className="flex mx-0 text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded">
+                    Track Order
+                  </button>
+                </div>
               </div>
             </div>
             <img
